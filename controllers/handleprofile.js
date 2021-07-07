90a635bf58e5f67f91409b2748ed2f0a14a04497
MehrezKanzari <kanzari.1993@gmail.com>
2021-07-07 19:31:09 +0100

    Post Api Profile!

--- controllers/handleprofile.js
+++ controllers/handleprofile.js
@@ -1,1 +1,17 @@
-const profile=(e,s,t)=>{const{id:o}=e.params;t.select("*").from("users").where({id:o}).then(e=>{if(0!==e.length)return s.json(e);s.status(400).json("not found!")}).catch(e=>s.status(400).json("error getting user!"))};module.exports={profile:profile};
+const profileGet=(e,s,t)=>{const{id:o}=e.params;t.select("*").from("users").where({id:o}).then(e=>{if(0!==e.length)return s.json(e);s.status(400).json("not found!")}).catch(e=>s.status(400).json("error getting user!"))};
+const profileUpdate = (req, res, db) => {
+  const { id } = req.params
+  const { name, age, pet } = req.body.formInput
+  db('users')
+  .where({ id })
+  .update({ name: name })
+  .then(resp => {
+    if (resp) {
+      res.json("success")
+    } else {
+      res.status(400).json('Not found')
+    }
+  })
+  .catch(err => res.status(400).json('error updating user'))
+}
+module.exports={profileGet:profileGet,profileUpdate:profileUpdate};
