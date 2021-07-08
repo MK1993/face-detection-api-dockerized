8f8e81d627dc1c64320086b8758513f6492f98c3
MehrezKanzari <kanzari.1993@gmail.com>
2021-07-08 20:43:21 +0100

    Sigin Updates!

--- controllers/handlesignin.js
+++ controllers/handlesignin.js
@@ -3,7 +3,10 @@ const jwt = require('jsonwebtoken');
 const redis = require('redis');
 
 // You will want to update your host to the proper address in production
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
 
 const signToken = (email) => {
   const jwtPayload = { email };
